83bee2a5c8b4aa0c035f5d2366c257e15f7ad010
kavyamg <kavyamg96@gmail.com>
2017-04-30 14:17:21 +0530

    first commit

---
+++ feature/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace feature
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doitallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.skinColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fillHoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blobToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aNDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cannyEdgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.skinColorToolStripMenuItem,
+            this.fillHoleToolStripMenuItem,
+            this.blobToolStripMenuItem,
+            this.aNDToolStripMenuItem,
+            this.cannyEdgeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(660, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.doitallToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // doitallToolStripMenuItem
+            // 
+            this.doitallToolStripMenuItem.Name = "doitallToolStripMenuItem";
+            this.doitallToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.doitallToolStripMenuItem.Text = "Doitall";
+            this.doitallToolStripMenuItem.Click += new System.EventHandler(this.doitallToolStripMenuItem_Click);
+            // 
+            // skinColorToolStripMenuItem
+            // 
+            this.skinColorToolStripMenuItem.Name = "skinColorToolStripMenuItem";
+            this.skinColorToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.skinColorToolStripMenuItem.Text = "Skin Color";
+            this.skinColorToolStripMenuItem.Click += new System.EventHandler(this.skinColorToolStripMenuItem_Click);
+            // 
+            // fillHoleToolStripMenuItem
+            // 
+            this.fillHoleToolStripMenuItem.Name = "fillHoleToolStripMenuItem";
+            this.fillHoleToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.fillHoleToolStripMenuItem.Text = "Fill Hole";
+            this.fillHoleToolStripMenuItem.Click += new System.EventHandler(this.fillHoleToolStripMenuItem_Click);
+            // 
+            // blobToolStripMenuItem
+            // 
+            this.blobToolStripMenuItem.Name = "blobToolStripMenuItem";
+            this.blobToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.blobToolStripMenuItem.Text = "Blob";
+            this.blobToolStripMenuItem.Click += new System.EventHandler(this.blobToolStripMenuItem_Click);
+            // 
+            // aNDToolStripMenuItem
+            // 
+            this.aNDToolStripMenuItem.Name = "aNDToolStripMenuItem";
+            this.aNDToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.aNDToolStripMenuItem.Text = "AND";
+            this.aNDToolStripMenuItem.Click += new System.EventHandler(this.aNDToolStripMenuItem_Click);
+            // 
+            // cannyEdgeToolStripMenuItem
+            // 
+            this.cannyEdgeToolStripMenuItem.Name = "cannyEdgeToolStripMenuItem";
+            this.cannyEdgeToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.cannyEdgeToolStripMenuItem.Text = "Canny Edge";
+            this.cannyEdgeToolStripMenuItem.Click += new System.EventHandler(this.cannyEdgeToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.13656F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.86344F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 272F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(660, 237);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 112);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(185, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(199, 112);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(390, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(267, 112);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(3, 121);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(176, 113);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox5.Location = new System.Drawing.Point(185, 121);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(199, 113);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox6.Location = new System.Drawing.Point(390, 121);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(267, 113);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openFolderToolStripMenuItem.Text = "openFolder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doitallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem skinColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fillHoleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blobToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aNDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cannyEdgeToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+    }
+}
+
---
+++ feature/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using AForge;
+using System.IO;
+
+namespace feature
+{
+    public partial class Form1 : Form
+    {
+        Bitmap originalImage;
+        Bitmap skinImage;
+        Bitmap monoImage;
+        Bitmap morphImage;
+        Bitmap biggestBlobsImage, k;
+        Bitmap resultImage, fd;
+        List<int> featureVector = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            var returnValue = openDialog.ShowDialog();
+            if (returnValue == DialogResult.OK)
+            {
+                //originalImage = new Bitmap(openDialog.FileName);
+                resultImage = new Bitmap(new Bitmap(openDialog.FileName), new Size(300, 300));
+                // pictureBox1.Image = originalImage;
+                skinImage = new Bitmap(new Bitmap(openDialog.FileName), new Size(300, 300)); //load,resize and assign to skin image
+                pictureBox1.Image = skinImage;
+            }
+
+        }
+
+        private void skinColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i, j, r, g, b, min, max;
+            for (i = 0; i < skinImage.Width; i++)
+            {
+                for (j = 0; j < skinImage.Height; j++)
+                {
+                    Color pixelColor = (skinImage.GetPixel(i, j));
+                    r = pixelColor.R;
+                    g = pixelColor.G;
+                    b = pixelColor.B;
+                    max = Math.Max(r, Math.Max(g, b));
+                    min = Math.Min(r, Math.Min(g, b));
+
+                    if (r > 95 && g > 40 && b > 20 && (max - min) > 15 && max == r && r - g > 15)
+                    {
+                        continue;
+                    }
+                    else
+                        skinImage.SetPixel(i, j, Color.Black);
+
+
+                }
+            }
+
+
+            pictureBox2.Image = skinImage;
+
+        }
+
+        private void fillHoleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create grayscale filter (BT709)
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            // apply the filter
+            Bitmap grayImage = filter.Apply(skinImage);
+
+            // create filter
+            Threshold filter2 = new Threshold(90);
+
+            filter2.ApplyInPlace(grayImage);
+            //monoImage = filter2.Apply(grayImage);
+            //pictureBox3.Image = monoImage;
+            Closing close = new Closing();
+            // apply the filter
+            morphImage = close.Apply(grayImage);
+            // create filter
+            Opening open = new Opening();
+            // apply the filter
+            k = open.Apply(morphImage);
+            pictureBox3.Image = k;
+
+
+        }
+
+        private void blobToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExtractBiggestBlob filter = new ExtractBiggestBlob();
+            // apply the filter
+            biggestBlobsImage = filter.Apply(k);
+            pictureBox4.Image = biggestBlobsImage;
+            IntPoint blobPosition = filter.BlobPosition;
+            Console.WriteLine(blobPosition.X);
+            Console.WriteLine(blobPosition.Y);
+            Console.WriteLine(biggestBlobsImage.Width);
+            Console.WriteLine(biggestBlobsImage.Height);
+            fd = new Bitmap(biggestBlobsImage);
+            int i, j;
+            for (i = 0; i < (biggestBlobsImage.Width); i++)
+            {
+                for (j = 0; j < (biggestBlobsImage.Height); j++)
+                {
+
+                    Color hg = (resultImage.GetPixel(i + blobPosition.X, j + blobPosition.Y));
+                    int r = fd.GetPixel(i, j).R;
+                    int g = fd.GetPixel(i, j).G;
+                    int b = fd.GetPixel(i, j).B;
+                    if ((r = g = b) == 255)
+                    {
+                        fd.SetPixel(i, j, hg);
+                    }
+                    else
+                        fd.SetPixel(i, j, Color.Black);
+
+
+                }
+            }
+
+        }
+
+        private void aNDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Image = fd;
+
+
+        }
+
+        
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+          /*  List<String> imagesList = new List<String>();
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            DialogResult result = folderDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+            {
+                DirectoryInfo directoryName = new DirectoryInfo(folderDialog.SelectedPath);
+                string dname = directoryName.ToString();
+                Console.WriteLine(directoryName);
+                string[] path = Directory.GetFiles(dname, "*");
+                List<Bitmap> images = new List<Bitmap>();
+                foreach(var path in path)
+                {
+                    Bitmap image = new Bitmap(path);
+                    images.Add(image);
+             
+                }
+
+
+                List<double[]> features = new List<double[]>();
+                foreach(var image in images)
+                {
+                    double[] features = automate(image);
+                    features.Add(feature);
+                }
+
+                double[][] featuresArray = features.ToArray();
+            } */
+        }
+
+        private void cannyEdgeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap grayImage = Grayscale.CommonAlgorithms.BT709.Apply(fd);
+            CannyEdgeDetector canny = new CannyEdgeDetector(0, 0, 1.4);
+            Bitmap edgeImage = canny.Apply(grayImage);
+
+            pictureBox6.Image = edgeImage;
+            // Console.WriteLine(edgeImage.Width);
+
+            int i, j, x, y, u, v;
+            double con1=0;
+            Color blackC = Color.FromArgb(Color.Black.ToArgb());
+            u = edgeImage.Width / 6;
+            v = edgeImage.Height / 6;
+            //Console.WriteLine(u);
+            //Console.WriteLine(v);
+
+            for (i = 0; i < 6; i++)
+            {
+                for (j = 0; j < 6; j++)
+                {
+                    int count = 0;
+                    for (x = i * u; x < ((i * u) + u); x++)
+                    {
+                        for (y = j * v; y < ((j * v) + v); y++)
+                        {
+
+                            Color c = (edgeImage.GetPixel(x, y));
+                            //Console.WriteLine(c);
+                            if (c != blackC)
+                            {
+                                count++;
+
+                                con1++;
+                            }
+
+
+                        }
+                    }
+                    
+                    featureVector.Add(count);
+                   //Console.WriteLine(count);
+
+                }
+            }
+           
+            
+            //Calculate Normalization and add the value to the featureNormVector
+            List<double> featureNormVector = new List<double>();
+
+            //Total Pixel Count
+            //Console.WriteLine(con1);
+
+            int z;
+            //Normalization
+            for (z = 0; z < featureVector.Count; z++)
+            {
+                double normalizedValue = featureVector[z] / con1;
+                Console.WriteLine(normalizedValue);
+                featureNormVector.Add(normalizedValue);
+            }
+
+
+        }
+
+        private void doitallToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            skinColorToolStripMenuItem_Click(sender, e);
+           
+            fillHoleToolStripMenuItem_Click(sender, e);
+            blobToolStripMenuItem_Click(sender, e);
+            aNDToolStripMenuItem_Click(sender,e);
+            cannyEdgeToolStripMenuItem_Click(sender, e);
+
+
+        }
+    }
+}
